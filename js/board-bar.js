f6be91525f5428b316eca4f01640c4aabc5a50ef
Bob Breznak <bob.breznak@gmail.com>
2016-04-21 07:16:09 -0400

    Update board-bar.js

--- js/board-bar.js
+++ js/board-bar.js
@@ -3,8 +3,7 @@
 var t = TrelloPowerUp.iframe();
 
 t.render(function(){
-  // this function we be called once on initial load
-  // and then called each time something changes that
-  // you might want to react to, such as new data being
-  // stored with t.set()
+  t.cards().then(function(cards) {
+    console.log("LISTS", cards);
+  });
 });
