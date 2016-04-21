89c156b9e009ef213ab2dd4aaea0c9a5fcca6fb1
Bob Breznak <bob.breznak@gmail.com>
2016-04-21 07:18:42 -0400

    Update board-bar.js

--- js/board-bar.js
+++ js/board-bar.js
@@ -6,7 +6,8 @@ t.render(function(){
   t.cards().then(function(cards) {
     console.log("CARDS", cards);
   });
-  
+});
+
 t.lists().then(function(lists) {
     console.log("LISTS", lists);
   });
