df70222e3f3dbbc9ee03d66eb75a2037be316291
Bob Breznak <bob.breznak@gmail.com>
2016-04-21 05:02:52 -0400

    Update template.js

--- js/template.js
+++ js/template.js
@@ -12,51 +12,17 @@ var pointScale = {
 };
 
 var getBadges = function(t){
-  return t.card('name')
-  .get('name')
-  .then(function(cardName){
-    var badgeColor;
-    var icon = GRAY_ICON;
-    var lowercaseName = cardName.toLowerCase();
-    if(lowercaseName.indexOf('green') > -1){
-      badgeColor = 'green';
-      icon = WHITE_ICON;
-    } else if(lowercaseName.indexOf('yellow') > -1){
-      badgeColor = 'yellow';
-      icon = WHITE_ICON;
-    } else if(lowercaseName.indexOf('red') > -1){
-      badgeColor = 'red';
-      icon = WHITE_ICON;
-    }
-
-    if(lowercaseName.indexOf('dynamic') > -1){
-      // dynamic badges can have their function rerun after a set number
-      // of seconds defined by refresh. Minimum of 10 seconds.
-      return [{
-        dynamic: function(){
-          return {
-            title: 'Detail Badge', // for detail badges only
-            text: 'Dynamic ' + (Math.random() * 100).toFixed(0).toString(),
-            icon: icon, // for card front badges only
-            color: badgeColor,
-            refresh: 10
-          }
-        }
-      }]
-    }
-
-    if(lowercaseName.indexOf('static') > -1){
-      // return an array of badge objects
-      return [{
+  return {
+    dynamic: function(){
+      return {
         title: 'Detail Badge', // for detail badges only
-        text: 'Static',
+        text: 'Dynamic ' + (Math.random() * 100).toFixed(0).toString(),
         icon: icon, // for card front badges only
-        color: badgeColor
-      }];
-    } else {
-      return [];
+        color: badgeColor,
+        refresh: 10
+      }
     }
-  })
+  }
 };
 
 var boardButtonCallback = function(t){
