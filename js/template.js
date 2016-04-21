90fa2cda986d15c6eda5fee6b48888f674923c2e
Bob Breznak <bob.breznak@gmail.com>
2016-04-21 04:59:28 -0400

    Update template.js

--- js/template.js
+++ js/template.js
@@ -3,20 +3,12 @@
 var WHITE_ICON = './images/icon-white.svg';
 var GRAY_ICON = './images/icon-gray.svg';
 
-var parkMap = {
-  acad: 'Acadia National Park',
-  arch: 'Arches National Park',
-  badl: 'Badlands National Park',
-  brca: 'Bryce Canyon National Park',
-  crla: 'Crater Lake National Park',
-  dena: 'Denali National Park',
-  glac: 'Glacier National Park',
-  grca: 'Grand Canyon National Park',
-  grte: 'Grand Teton National Park',
-  olym: 'Olympic National Park',
-  yell: 'Yellowstone National Park',
-  yose: 'Yosemite National Park',
-  zion: 'Zion National Park'
+var pointScale = {
+  'X-Small' : 1,
+  'Small'   : 2,
+  'Medium'  : 4,
+  'Large'   : 8,
+  'X-Large' : 16
 };
 
 var getBadges = function(t){
@@ -67,18 +59,6 @@ var getBadges = function(t){
   })
 };
 
-var formatNPSUrl = function(t, url){
-  if(!/^https?:\/\/www\.nps\.gov\/[a-z]{4}\//.test(url)){
-    return null;
-  }
-  var parkShort = /^https?:\/\/www\.nps\.gov\/([a-z]{4})\//.exec(url)[1];
-  if(parkShort && parkMap[parkShort]){
-    return parkMap[parkShort];
-  } else{
-    return null;
-  }
-};
-
 var boardButtonCallback = function(t){
   return t.popup({
     title: 'Popup List Example',
@@ -112,13 +92,12 @@ var boardButtonCallback = function(t){
 };
 
 var cardButtonCallback = function(t){
-  var items = Object.keys(parkMap).map(function(parkCode){
-    var urlForCode = 'http://www.nps.gov/' + parkCode + '/';
+  var items = Object.keys(pointScale).map(function(pointCode){
     return {
-      text: parkMap[parkCode],
-      url: urlForCode,
+      text: pointCode,
+      cost: pointScale[pointCode],
       callback: function(t){
-        return t.attach({ url: urlForCode, name: parkMap[parkCode] })
+        return t.set('card', 'shared', { 'pointScale' : pointCode, 'points' : pointScale[pointCode] })
         .then(function(){
           return t.closePopup();
         })
@@ -138,57 +117,6 @@ var cardButtonCallback = function(t){
 };
 
 TrelloPowerUp.initialize({
-  'attachment-sections': function(t, options){
-    // options.entries is a list of the attachments for this card
-    // you can look through them and 'claim' any that you want to
-    // include in your section.
-
-    // we will just claim urls for Yellowstone
-    var claimed = options.entries.filter(function(attachment){
-      return attachment.url.indexOf('http://www.nps.gov/yell/') == 0;
-    });
-
-    // you can have more than one attachment section on a card
-    // you can group items together into one section, have a section
-    // per attachment, or anything in between.
-    if(claimed && claimed.length > 0){
-      // if the title for your section requires a network call or other
-      // potentially length operation you can provide a function for the title
-      // that returns the section title. If you do so, provide a unique id for
-      // your section
-      return [{
-        id: 'Yellowstone', // optional if you aren't using a function for the title
-        claimed: claimed,
-        icon: GRAY_ICON,
-        title: 'Example Attachment Section: Yellowstone',
-        content: {
-          type: 'iframe',
-          url: t.signUrl('./section.html', { arg: 'you can pass your section args here' }),
-          height: 230
-        }
-      }];
-    } else {
-      return [];
-    }
-  },
-  'attachment-thumbnail': function(t, options){
-    var parkName = formatNPSUrl(t, options.url);
-    if(parkName){
-      // return an object with some or all of these properties:
-      // url, title, image, openText, modified (Date), created (Date), createdBy, modifiedBy
-      return {
-        url: options.url,
-        title: parkName,
-        image: {
-          url: './images/nps.svg',
-          logo: true // false if you are using a thumbnail of the content
-        },
-        openText: 'Open in NPS'
-      };
-    } else {
-      throw t.NotHandled();
-    }
-  },
   'board-buttons': function(t, options){
     return [{
       icon: WHITE_ICON,
@@ -208,28 +136,6 @@ TrelloPowerUp.initialize({
   },
   'card-detail-badges': function(t, options) {
     return getBadges(t);
-  },
-  'card-from-url': function(t, options) {
-    var parkName = formatNPSUrl(t, options.url);
-    if(parkName){
-      return {
-        name: parkName,
-        desc: 'An awesome park: ' + options.url
-      };
-    } else {
-      throw t.NotHandled();
-    }
-  },
-  'format-url': function(t, options) {
-    var parkName = formatNPSUrl(t, options.url);
-    if(parkName){
-      return {
-        icon: GRAY_ICON,
-        text: parkName
-      };
-    } else {
-      throw t.NotHandled();
-    }
   },
   'show-settings': function(t, options){
     return t.popup({
