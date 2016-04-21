30f2425539d2f8bb25b5f758d66163aa963f4bc5
Bob Breznak <bob.breznak@gmail.com>
2016-04-21 07:29:12 -0400

    Update board-bar.js

--- js/board-bar.js
+++ js/board-bar.js
@@ -4,7 +4,7 @@ var t = TrelloPowerUp.iframe();
 
 t.render(function() {
   t.lists().then(function(lists) {
-    lists[0].cards().then(function(cards) {
+    lists[0].get('cards').then(function(cards) {
       console.log("CARDS", cards);
     });
   });
